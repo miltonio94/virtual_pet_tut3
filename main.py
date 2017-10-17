927b6600c7e076d864cc741bb6747d3b9eaacd6c
Mr. M <miltonio@Miltons-MacBook-Pro.local>
2017-10-17 15:52:44 +0100

    wip

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def page():
-    return "render_template('index.html')"
+    return render_template('index.html')
 
 if LOCAL and __name__ == "__main__":
     app.run(debug=True)
